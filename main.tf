ef40e896444a40b415d8ab0eade9e2402cc32250
AleksFirsta <bolgarwow@gmail.com>
2024-09-07 13:55:38 +0300

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,36 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "apigateway-v2" {
+  source  = "terraform-aws-modules/apigateway-v2/aws"
+  version = "5.2.0"
+# Disable creation of the API and all resources
+  create = true
+  name = "test"
+
+  # Disable creation of the domain name and API mapping
+  create_domain_name = false
+
+  # Disable creation of Route53 alias record(s) for the custom domain
+  create_domain_records = false
+
+  # Disable creation of the ACM certificate for the custom domain
+  create_certificate = false
+
+  # Disable creation of the routes and integrations
+  create_routes_and_integrations = false
+
+  # Disable creation of the stage
+  create_stage = false
+
+}
